78e143f34e036014dd08a5556450c3040678aeee
bartmanus <bartmanus@users.noreply.github.com>
2016-12-10 16:57:48 +0100

    Refactor taxicab solution to increase code reusability
    Modularize code by separating solution from argument handling

--- 2016/day_1-no_time_for_a_taxicab.py
+++ 2016/day_1-no_time_for_a_taxicab.py
@@ -25,23 +25,29 @@ Implementation:
 import sys
 
 def new_state(current_state, direction):
+    """
+    Calculates new heading from current heading and relative direction.
+    """
     new_axis = (current_state['axis']+1) & 1
     multiplier = -1 if current_state['axis'] == 0 and direction == 1 or \
                        current_state['axis'] == 1 and direction == -1 else 1 
     new_sign = current_state['sign'] * multiplier
     return {'axis': new_axis, 'sign': new_sign}
 
-if __name__ == '__main__':
+def taxicab(instructions):
     DIRECTION_MAPPING = {'R':1, 'L':-1}
     origin_vector = [0, 0]
     state = {'axis': 0, 'sign': +1}
-    instructions = sys.argv[1] if len(sys.argv) == 2 \
-                               else input('Instructions, please: ')
     for instruction in instructions.split(', '):
         direction = DIRECTION_MAPPING[instruction[0]]
         steps = int(instruction[1:])
         state = new_state(state, direction)
         origin_vector[state['axis']] += steps * state['sign']
     distance = sum((abs(s) for s in origin_vector))
-    print('Taxicab distance is {}.'.format(distance))
+    return distance
+        
+if __name__ == '__main__':
+    instructions = sys.argv[1] if len(sys.argv) == 2 \
+                               else input('Instructions, please: ')
+    print('Taxicab distance is {}.'.format(taxicab(instructions)))
 
